f411808cac7a1d496f640659f37309783cae1eeb
NurZorlu <nurzada.zholomanova.ch@gmail.com>
2023-10-10 19:23:32 +0300

    first_commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import './Button.css'
+
+export function Button(){
+    return (
+        <div>
+            <button className="button">Advanced Search</button>
+        </div>
+    )
+}
---
+++ src/components/Search.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import './Search.css'
+
+export function Search(){
+    return(
+        <div>
+            <input className='search' type="text" placeholder="Search" />
+        </div>
+    )
+}
